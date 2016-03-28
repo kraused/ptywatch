68e9bde8e75d8e27392a8161561d015ad3d6c687
Dorian Krause <github@91df63fb.com>
2016-03-28 12:30:50 +0200

    Handle received messages asynchronously

--- dbus-signal/wait4signal.c
+++ dbus-signal/wait4signal.c
@@ -2,12 +2,75 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <pthread.h>
 
 #include <glib.h>
 #include <gio/gio.h>
 
 #include "common.h"
 #include "error.h"
+
+struct Message
+{
+	char	*origin;
+	char	*summary;
+	char	*body;
+};
+
+void *Thread_Start_Routine(void *arg)
+{
+	struct Message *msg = (struct Message *)arg;
+
+        fprintf(stdout, "Origin : %s\n", msg->origin);
+        fprintf(stdout, "Summary: %s\n", msg->summary);
+	/* +1 to get rid of the prepended \r of the wall message.
+	 */
+        fprintf(stdout, "Body   : \"%s\"\n", msg->body + 1);
+
+	free(msg->origin);
+	free(msg->summary);
+	free(msg->body);
+	free(msg);
+
+	return NULL;
+}
+
+SInt32 Create_Detached_Thread(struct Message *msg)
+{
+	SInt32 err;
+	pthread_attr_t attr;
+	pthread_t thr;
+
+	err = pthread_attr_init(&attr);
+	if (UNLIKELY(err)) {
+		errno = err;
+		PTYWATCH_ERROR("pthread_attr_init() failed with error %d (%s)", errno, strerror(errno));
+		return -err;
+	}
+
+	err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	if (UNLIKELY(err)) {
+		errno = err;
+		PTYWATCH_ERROR("pthread_attr_setdetachstate() failed with error %d (%s)", errno, strerror(errno));
+		return -err;
+	}
+
+	err = pthread_create(&thr, &attr, Thread_Start_Routine, (void *)msg);
+	if (UNLIKELY(err)) {
+		errno = err;
+		PTYWATCH_ERROR("pthread_create() failed with error %d (%s)", errno, strerror(errno));
+		return -err;
+	}
+
+	err = pthread_attr_destroy(&attr);
+	if (UNLIKELY(err)) {
+		errno = err;
+		PTYWATCH_ERROR("pthread_attr_destroy() failed with error %d (%s)", errno, strerror(errno));
+	}
+
+	return 0;
+}
 
 void On_Caught_Signal(GDBusConnection *connection,
                       const gchar *sender_name,
@@ -19,15 +82,22 @@ void On_Caught_Signal(GDBusConnection *connection,
 {
 	GVariant *summary;
 	GVariant *body;
+	struct Message *msg;
 
 	summary = g_variant_get_child_value(parameters, 0);
 	body    = g_variant_get_child_value(parameters, 1);
 
-        fprintf(stdout, "Origin : %s\n", object_path);
-        fprintf(stdout, "Summary: %s\n", g_variant_get_string(summary, NULL));
-	/* +1 to get rid of the prepended \r of the wall message.
-	 */
-        fprintf(stdout, "Body   : \"%s\"\n", g_variant_get_string(body, NULL) + 1);
+	msg = malloc(sizeof(struct Message));
+	if (UNLIKELY(!msg)) {
+		PTYWATCH_ERROR("malloc() failed.");
+		return;
+	}
+
+	msg->origin  = strdup(object_path);
+	msg->summary = strdup(g_variant_get_string(summary, NULL));
+	msg->body    = strdup(g_variant_get_string(body, NULL));
+
+	(void )Create_Detached_Thread(msg);
 
 	g_variant_unref(summary);
 	g_variant_unref(body);
